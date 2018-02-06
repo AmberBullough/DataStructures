416139ae1012685b22bb0ef9cb52103b7ddde1c4
Amber Bullough <amber@bertbullough.com>
2018-02-06 09:50:21 -0700

    Finished node

--- DataStructures/Model/Structures/Nodes/Node.hpp
+++ DataStructures/Model/Structures/Nodes/Node.hpp
@@ -11,4 +11,49 @@
 
 #include <stdio.h>
 
+template <class Type>
+class Node
+{
+protected:
+    Type data;
+    
+public:
+    Node();
+    Node(Type data);
+    void setData(Type data);
+    Type getData();
+};
+
+template <class Type>
+Node<Type> :: Node()
+{
+    //Neede for default construction compilation
+    // since when we create a structure itdoes not have an item to store yet.
+}
+
+template <class Type>
+Node<Type> :: Node(Type data)
+{
+    this -> data = data;
+}
+
+template <class Type>
+void Node<Type>  :: setData (Type data)
+{
+    this -> data = data;
+}
+
+template <class Type>
+Tupe Node <Type> :: getData()
+{
+    return data;
+}
+
+
+
+
+
+
+
+
 #endif /* Node_hpp */
