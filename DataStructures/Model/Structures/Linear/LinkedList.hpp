197022c1092c46b9ee4fd773e82c2ac0c57ad963
Amber Bullough <amber@bertbullough.com>
2018-02-14 08:42:26 -0700

    Added in linear node

--- DataStructures/Model/Structures/Linear/LinkedList.hpp
+++ DataStructures/Model/Structures/Linear/LinkedList.hpp
@@ -9,5 +9,58 @@
 #ifndef LinkedList_h
 #define LinkedList_h
 
+void LinkedList<Type> :: add(Type item)
+{
+    LinearNode <Type> * newData = newLinearNode<Type> (item);
+    
+    if(size == 0)
+    {
+        this->front = newData;
+    }
+    else
+    {
+        this-> end -> setNextNode(newData);
+    }
+    this -> end = newData;
+    this->size += 1;
+}
+
+template <classType>
+void LinkedList<Type> :: addAtIndex(int index, Type item)
+{
+    assert(index >= 0 && index <= size);
+    if index == size)
+    {
+        add(item);
+    }
+    else
+    {
+        LinearNode<Type> * toBeAdded = new LinearNode<Type>(item);
+        if(index == 0)
+        {
+            toBeAdded -> setNextNode(front);
+            front = toBeAdded;
+        }
+        else
+        {
+            LinearNode<Type> * previous = nullptr;
+            LinearNode<Type> * current = front;
+            for (int psoition = 0; position < index ; index ++)
+            {
+                previous = current;
+                current = current -> getNextNode();
+            }
+            previous -> setNextNode(toBeAdded);
+            toBeAdded -> setNextNode(current);
+        }
+        size ++;
+    }
+}
+
+template<class Type>
+Type LinkedList<Type> :: remove(int index)
+{
+    
+}
 
 #endif /* LinkedList_h */
---
+++ DataStructures/Model/Structures/Nodes/LinearNode.hpp
@@ -0,0 +1,33 @@
+//
+//  LinearNode.hpp
+//  DataStructures
+//
+//  Created by Bullough, Amber on 2/14/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef LinearNode_hpp
+#define LinearNode_hpp
+
+#include <assert.h>
+#include "../../Nodes/LinearNode.hpp"
+
+class LinearNode
+{
+protected:
+    int size;
+    
+public:
+    //Structure
+    virtual void add(Type item) = 0;
+    virtual void addAtIndex(int index, TYpe item) = 0;
+    virtyal Type remove(int index) = 0;
+    virtual Type getFromIndex(int index) = 0;
+    //Helper
+    virtal int getSize() coonst = 0;
+    virtual linearNode<Type> * getFront() = 0;
+    virtual LinearNode<Type> * getEnd() = 0;
+    
+}
+
+#endif /* LinearNode_hpp */
