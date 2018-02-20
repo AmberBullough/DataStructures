042092a78d4325c8305d4027e5f3ea0a7dc000e0
Amber Bullough <amber@bertbullough.com>
2018-02-20 09:47:38 -0700

    Added in linked tests Need to finish .hpp one

--- DataStructures/Controller/FileController.cpp
+++ DataStructures/Controller/FileController.cpp
@@ -41,3 +41,34 @@ vector<CrimeData> FileController :: readCrimeDataToVector(string filename)
     return crimeVector;
 }
 
+LinkedList<CrimeDatafileCOntroller :: readDataToList(string fileName)
+{
+    LinkedList<CrimeData> crimes;
+    
+    string currentCSVLine;
+    int rowCount = 0;
+    ifstream dataFile(fileName);
+    
+    if(dataFile.is_open())
+    {
+        while(!dataFile.eof())
+        {
+            getLine(dataFile,currentCSVLine, '/r');
+            if(rowCount != 0)
+            {
+                if(currentCSVLine.length() != 0)
+                {
+                    CrimeData row(currentCSVLine);
+                    crimes.add(row);
+                }
+            }
+            rowCount++;
+        }
+        dataFile.close();
+    }
+    else
+    {
+        cerr << "NO FILE" << endl;
+    }
+    return crimes;
+}
--- DataStructures/Testers/LinkedListTester.cpp
+++ DataStructures/Testers/LinkedListTester.cpp
@@ -6,4 +6,20 @@
 //  Copyright © 2018 CTEC. All rights reserved.
 //
 
-#include "LinkedListTester.hpp"
+#include LinkedListTester_hpp
+#define LinkedListTester_hpp
+
+#include "../Model/Structures/Linear/LinkedList.hpp"
+#include "../Controller/FileController.hpp"
+#include "../Controller/Tools/Time.hpp"
+#include "../Data/CrimeData.hpp"
+#include <iostream>
+
+class LinkedListTester
+{
+public:
+    void testListBasics();
+    void testListWithData();
+};
+
+#endif /* LinkedListTester_hpp*/
