fe36fbb12e25afa7ed9930c31fac09553b867303
Amber Bullough <amber@bertbullough.com>
2018-02-22 09:48:33 -0700

    Finised stack hpp

---
+++ DataStructures/Model/Structures/Linear/Linear.hpp
@@ -0,0 +1,14 @@
+//
+//  Linear.hpp
+//  DataStructures
+//
+//  Created by Bullough, Amber on 2/22/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Linear_hpp
+#define Linear_hpp
+
+#include <stdio.h>
+
+#endif /* Linear_hpp */
