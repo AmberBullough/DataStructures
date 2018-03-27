eca26c8325536cf086c6d74f4ce74df68547a851
Amber Bullough <amber@bertbullough.com>
2018-03-27 09:55:22 -0600

    Finished hpp

--- DataStructures/Model/Structures/Nodes/BinaryTreeNode.hpp
+++ DataStructures/Model/Structures/Nodes/BinaryTreeNode.hpp
@@ -35,4 +35,48 @@ public:
     
     
 };
+
+template <class Type>
+BinaryTreeNode<Type>:: BinaryTreeNode() : Node<Type>()
+{
+    root = nullptr;
+    right = nullptr;
+    left = nullptr;
+}
+
+template <class Type>
+BinaryTreeNode<Type> * BinaryTreeNode<Type> :: getRootNode()
+{
+    return this->root;
+}
+
+template <class Type>
+BinaryTreeNode<Type> * BinaryTreeNode<Type> :: getRightNode()
+{
+    return this-> right;
+}
+
+template <class Type>
+BinaryTreeNode<Type> * BinaryTreeNode<Type> :: getLeftNode()
+{
+    return this-> left;
+}
+
+template <class Type>
+void BinaryTreeNode<Type>:: setRootNode(BinaryTreeNode<Type> * newRoot)
+{
+    root = newRoot;
+}
+
+template <class Type>
+void BinaryTreeNode<Type>:: setRightNode(BinaryTreeNode<Type> * right)
+{
+    this -> right = right;
+}
+
+template <class Type>
+void BinaryTreeNode<Type>:: setLeftNode(BinaryTreeNode<Type> * left)
+{
+    this->left = left;
+}
 #endif /* BinaryTreeNode_hpp */
