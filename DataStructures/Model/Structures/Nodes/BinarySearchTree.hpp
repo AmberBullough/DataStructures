378755c04386d8faf8ed7a441f40e3c4c9b2567e
Amber Bullough <amber@bertbullough.com>
2018-03-29 08:39:58 -0600

    Finsihed declarations and insert class

--- DataStructures/Model/Structures/Nodes/BinarySearchTree.hpp
+++ DataStructures/Model/Structures/Nodes/BinarySearchTree.hpp
@@ -9,8 +9,10 @@
 #ifndef BinarySearchTree_hpp
 #define BinarySearchTree_hpp
 
-#include "Node.hpp"
+#include "Tree.hpp"
 #include "BinaryTreeNode.hpp"
+#include <iostream>
+using namespace std;
 
 template <class Type>
 class BinarySearchTree : public Tree<Type>
@@ -41,11 +43,70 @@ public:
     void preOrderTransversal();
     void postOrderTransversal();
     void demoTransversalSteps(BinaryTreeNode<Type> * node);
-   
+
+    int getSize();
+    int getHeight();
+    bool isComplete();
+    bool isBalanced();
     
-private:
+    bool contains(Type value);
+    void insert(Type itemToInsert);
+    void remove(Type value);
     
+    Type findMinimum();
+    Type findMaximum();
     
 };
 
+template <class Type>
+BinarySearchTree<Type> :: BinarySearchTree()
+{
+    this->root = nullptr;
+}
+
+template <class Type>
+void BinarySearchTree<Type> :: insert(Type itemToInsert)
+{
+    BinaryTreeNode<Type> * insertMe = new BinaryTreeNode<Type>(itemToInsert);
+    BinaryTreeNode<Type> * previous = nullptr;
+    BinaryTreeNode<Type> * current = this->root;
+    
+    if(current == nullptr)
+    {
+        this->root = insertMe;
+    }
+    else
+    {
+        while(current != nullptr)
+        {
+            previous = current;
+            if(itemToInsert < current-> getData())
+            {
+                current - current->getLeftNode();
+            }
+            else if(itemToInsert > current->getData())
+            {
+                current = current->getRightNode();
+            }
+            else //Remove cerr after verfication of understanding
+            {
+                cerr<< "item Exists already - Exiting insert" << endl;
+                delete insertMe;
+                return; // Okay becayse it is returning nothing
+            }
+        }
+        if (previous->getData() > itemToInsert)
+        {
+            previous -> setLeftNode(insertMe);
+        }
+        else
+        {
+            previous->setRightNode(insertMe);
+        }
+        insertMe->setRightNode(previous);
+        }
+}
+    
+    
+    
 #endif /* BinarySearchTree_hpp */
---
+++ DataStructures/Model/Structures/Nodes/Tree.hpp
@@ -0,0 +1,14 @@
+//
+//  Tree.hpp
+//  DataStructures
+//
+//  Created by Bullough, Amber on 3/29/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Tree_hpp
+#define Tree_hpp
+
+#include <stdio.h>
+
+#endif /* Tree_hpp */
