85ca2282663fe9819c26c4a558b36cebf1c80f9b
Amber Bullough <amber@bertbullough.com>
2018-04-09 09:30:17 -0600

    Added in tree need to fix thebinary tree search

---
+++ DataStructures/Testers/BinaryTreeTester.hpp
@@ -0,0 +1,25 @@
+//
+//  BinaryTreeTester.hpp
+//  DataStructures
+//
+//  Created by Bullough, Amber on 3/29/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef BinaryTreeTester_hpp
+#define BinaryTreeTester_hpp
+
+#include "../Model/Structures/Nodes/BinaryTreeNode.hpp"
+#include "../Model/Structures/NonLinear/BinarySearchTree.hpp"
+#include <iostream>
+
+class BinaryTreeTester
+{
+private:
+    BinarySearchTree<int> testTree;
+public:
+    void doTreeStuff();
+};
+
+
+#endif /* BinaryTreeTester_hpp */
