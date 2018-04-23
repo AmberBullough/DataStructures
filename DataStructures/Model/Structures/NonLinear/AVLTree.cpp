aa5971f5f029fcfc28dbfab3d8f2b3bb0fb11fc4
Amber Bullough <amber@bertbullough.com>
2018-04-23 08:47:38 -0600

    Finished binarySearchTree from canvas

---
+++ DataStructures/Model/Structures/NonLinear/AVLTree.cpp
@@ -0,0 +1,9 @@
+//
+//  AVLTree.cpp
+//  DataStructures
+//
+//  Created by Bullough, Amber on 4/17/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "AVLTree.hpp"
---
+++ DataStructures/Model/Structures/NonLinear/AVLTree.hpp
@@ -0,0 +1,129 @@
+//
+//  AVLTree.hpp
+//  DataStructures
+//
+//  Created by Bullough, Amber on 4/17/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef AVLTree_hpp
+#define AVLTree_hpp
+
+#include "BinarySearchTree.hpp"
+template <class Type>
+class AVLTree : public BinarySearchTree<Type>
+{
+private:
+    
+    BinaryTreeNode<Type> * leftRotation(BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * rightRotation(BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * leftRightRotation(BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * rightLeftRotation(BinaryTreeNode<Type> * parent);
+    
+    BinaryTreeNode<Type> * balanceSubTree(BinaryTreeNode<Type> * parent);
+    
+    BinaryTreeNode<Type> * insertNode(BinaryTreeNode<Type> * parent, Type value);
+    BinaryTreeNode<Type> * removeNode(BinaryTreeNode<Type> * parent, Type value);
+    
+    int heightDifference(BinaryTreeNode<Type> * parent);
+    
+public:
+    AVLTree();
+    
+    void insert(Type itemToInsert);
+    void remove(Type value);
+    
+};
+
+/*
+ A negative value means the right is greater than the left
+ */
+template <class Type>
+int AVLTree<Type> :: heightDifference(BinaryTreeNode<Type> * node)
+{
+    int balance;
+    int leftHeight = this-> calculateHeight(node-> getLeftNode());
+    int rightHeight = this -> calculateHeight(node-> getRightNode());
+    balance = leftHeight - rightHeight;
+    return balance;
+}
+
+template <class Type>
+BinaryTreeNode<Type>* AVLTree<Type> :: leftRotation (BinaryTreeNode<Type> * parent)
+{
+    BinaryTreeNode<Type> * changedNode;
+    changedNode = parent -> getLeftNode();
+    
+    parent->setLeftChild(changedNode->getRightNode());
+    
+    changedNode->setRightChild(parent);
+    
+    return changedNode;
+}
+
+template <class Type>
+BinaryTreeNode<Type>* AVLTree<Type> :: rightRotation (BinaryTreeNode<Type> * parent)
+{
+    BinaryTreeNode<Type> * changedNode;
+    changedNode = parent -> getRightNode();
+    
+    parent->setRightChild(changedNode->getLeftNode());
+    
+    changedNode->setRightChild(parent);
+    
+    return changedNode;
+}
+
+template <class Type>
+BinaryTreeNode<Type>* AVLTree<Type> :: rightLeftRotation (BinaryTreeNode<Type> * parent)
+{
+    BinaryTreeNode<Type> * changedNode;
+    changedNode = parent -> getRightNode();
+    
+    parent->setRightChild(leftRotation(changedNode));
+    
+    return rightRotation(parent);
+}
+
+template <class Type>
+BinaryTreeNode<Type>* AVLTree<Type> :: leftRightRotation (BinaryTreeNode<Type> * parent)
+{
+    BinaryTreeNode<Type> * changedNode;
+    changedNode = parent -> getLeftNode();
+    
+    parent->setLeftChild(rightRotation(changedNode));
+    
+    return leftRotation(parent);
+}
+
+template <class Type>
+BinaryTreeNode<Type>* AVLTree<Type> :: balanceSubTree (BinaryTreeNode<Type> * parent)
+{
+    int balanceFactor = heightDifference(parent);
+    
+    if(balanceFacot > 1)
+    {
+        if(heightDifference(parent->getLeftNode()) > 0)
+        {
+            parent = leftRotation(parent);
+        }
+        else
+        {
+            parent = leftRightRotation(parent);
+        }
+    }
+    else if(balaceFacor < -1)
+    {
+        if(heightDifference(parent->getRightNode()) >0)
+        {
+            parent = rightleftRotation(parent);
+        }
+        else
+        {
+            parent = rightRotation(parent);
+        }
+    }
+    return parent;
+}
+
+#endif /* AVLTree_hpp */
