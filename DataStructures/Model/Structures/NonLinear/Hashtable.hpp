37ebaa56b616606567cd821516b88bcd521938cb
Amber Bullough <amber@bertbullough.com>
2018-05-01 09:24:59 -0600

    FINISHED hash table for today

--- DataStructures/Model/Structures/NonLinear/Hashtable.hpp
+++ DataStructures/Model/Structures/NonLinear/Hashtable.hpp
@@ -9,6 +9,117 @@
 #ifndef Hashtable_hpp
 #define Hashtable_hpp
 
-#include <stdio.h>
+#include "../Nodes/HashNode.hpp"
+
+
+template <class Type>
+class Hashtable
+{
+private:
+    HashNode<Type> ** internalStorage;
+    long size;
+    long capacity;
+    double loadFactor;
+    
+    long getNextPrime();
+    bool isPrime(long current);
+    void resize();
+    long findPosition(HashNode<Type>*insertedNode);
+    long handleCollision(HashNode<Type>* insertedNode, long index);
+    
+public:
+    Hashtable();
+    ~Hashtable();
+
+    void insert(Type data);
+    long getSize();
+};
+
+template <class Type>
+Hashtable<Type> :: Hashtable()
+{
+    capacity = 101;
+    internalStorage = new HashNode<Type>*[capacity];
+    size = 0;
+    loadFactor = .80;
+}
+
+template <class Type>
+Hashtable<Type> :: ~Hashtable()
+{
+    delete [] internalStorage;
+}
+
+template <class Type>
+bool Hashtable<Type> :: isPrime(long current)
+{
+    if(canidateNumber <= 1)
+    {
+        return false;
+    }
+    else if (current == 2 || current == 3)
+    {
+        return true;
+    }
+    else if (current % 2 == 0)
+     {
+         return false;
+     }
+    else
+    {
+        for(int next = 3; next <= sqrt(current) +1; next +=2)
+        {
+            if(current % next == 0)
+            {
+                return false;
+            }
+        }
+    return true;
+    }
+}
+
+template <class Type>
+long Hashtable<Type> :: findPosition(HashNode<Type> * insert)
+{
+    long insertPosition = insert->getKey() % this->capacity;
+    return insertPosition;
+}
+
+template <class Type>
+long Hashtable<Type> :: handleCollision(HashNode<Type> * current, long index)
+{
+    
+}
+
+template <class Type>
+long Hashtable<Type> :: getSize()
+{
+    
+}
+
+template <class Type>
+long Hashtable<Type> :: getNextPrime()
+{
+    long nextPrime = (this->capacity * 2) + 1;
+    
+    while(!isPrime(nextPrime))
+    {
+        nextPrime += 2;
+    }
+    return nextPrime;
+    
+}
+
+template <class Type>
+void Hashtable<Type> :: resize()
+{
+    
+}
+
+template <class Type>
+void Hashtable<Type> :: insert(Type value)
+{
+    
+}
 
 #endif /* Hashtable_hpp */
